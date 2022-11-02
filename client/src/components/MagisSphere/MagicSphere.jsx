de2dee5f362cd1726c9cb1f6f9ef7a99b0cb45fb
Johnkhv69 <johnkhv69@gmail.com>
2022-11-02 14:22:58 +0500

    sphere spins

--- client/src/components/MagisSphere/MagicSphere.jsx
+++ client/src/components/MagisSphere/MagicSphere.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable */ 
 import React from 'react'
+import styles from './Magic.module.css';
 
 function MagicSphere() {
     return (
-        <div className='styles.magicSphere'>MagicSphere
-            <img src="Magic_Sphere_007.png" alt="magic sphere" />
+        <div className={styles.magicSphere}>
+            <img src="Magic_Sphere_007.png" height='100%' width='100%' alt="magic sphere" />
         </div>
     )
 }
