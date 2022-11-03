03aa04826a3365d666ed9c8f9faca32d6bc7e950
Johnkhv69 <johnkhv69@gmail.com>
2022-11-03 15:55:57 +0500

    css terrible

---
+++ client/src/components/Greeting/Greeting.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import React from 'react'
+import styles from './Greeting.module.css';
+
+
+export default function Greeting() {
+    return (
+        <div className={styles.Greeting}>
+            Greeting
+        </div>
+    )
+}
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,13 +1,20 @@
+/* eslint-disable */
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Greeting from '../Greeting/Greeting';
 import MagicSphere from '../MagisSphere/MagicSphere';
+import PreArticles from '../PreArticles/PreArticles';
+import styles from './Home.module.css';
+
 
 export default function Home() {
   const { userInfo } = useSelector((state) => state.user);
   return (
-    <div>
+    <div className={styles.home}>
       <h2>{userInfo ? `Hello, ${userInfo.user_name}!` : 'Welcome!'}</h2>
+      <Greeting/>
       <MagicSphere />
+      <PreArticles />
     </div>
   );
 }
