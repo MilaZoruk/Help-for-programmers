135f1af51c6441aca4c55b2a14a32e0b8ae7c0fc
Lyudmila <zoruk.ll@gmail.com>
2022-11-04 15:23:11 +0300

    add harvard room

--- client/src/components/RelaxRoom/RoomHarvard/RoomHarvard.jsx
+++ client/src/components/RelaxRoom/RoomHarvard/RoomHarvard.jsx
@@ -1,17 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import ReactPlayer from 'react-player'
+
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 import styles from './RoomHarvard.module.css';
 
 export default function RoomHarvard() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
-  const [image, setImage] = useState('');
+  const [onInit, setOnInit] = useState('');
 
   useEffect(() => {
     async function getBook() {
       const resp = await fetch(
-        'https://api.harvardartmuseums.org/image?apikey=7039d293-f3bf-432e-a15a-dbafdd6f6a9a&size=25',
+        'https://api.harvardartmuseums.org/image?apikey=7039d293-f3bf-432e-a15a-dbafdd6f6a9a&size=15',
         {
           method: 'GET',
           headers: {
@@ -33,24 +37,24 @@ export default function RoomHarvard() {
     return <div>Loading...</div>;
   }
   return (
-    <ul>
-     <div className='player-wrapper'>
-        <ReactPlayer
-          className='react-player'
-          url={['https://www.youtube.com/watch?v=3SeHZShpkxU', 'https://www.youtube.com/watch?v=YH7-TCwOHtg', 'https://www.youtube.com/watch?v=Bl7Jp5oDe6o']}
-          width='100%'
-        />
-      </div>
-      {items.map((el) => (
-        
-        <div className={styles.allArt}>
-            <img key={el.id}
-              className={styles.imgARt}
-              src={el.baseimageurl}
-              alt={el.technique}
-            />
-          </div>
-      ))}
+     <ul><div className='player-wrapper'>
+          <ReactPlayer
+              className='react-player'
+              url={['https://www.youtube.com/watch?v=3SeHZShpkxU', 'https://www.youtube.com/watch?v=YH7-TCwOHtg', 'https://www.youtube.com/watch?v=Bl7Jp5oDe6o']}
+              width='100%' />
+      </div> 
+      <div className={styles.carousel}>
+      <Carousel autoPlay>
+                  {items.map((el) => (
+                      <div className={styles.allArt}>
+                          <img key={el.id}
+                              className={styles.imgARt}
+                              src={el.baseimageurl}
+                              alt={el.technique} />
+                      </div>
+                  ))}
+        </Carousel>
+        </div>
     </ul>
   );
 }
