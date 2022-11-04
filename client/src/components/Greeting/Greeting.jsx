2e6a028075e14bca5660959bdc42882abb89bb0f
Johnkhv69 <johnkhv69@gmail.com>
2022-11-04 12:39:45 +0500

    css mistakes fix

--- client/src/components/Greeting/Greeting.jsx
+++ client/src/components/Greeting/Greeting.jsx
@@ -6,7 +6,7 @@ import styles from './Greeting.module.css';
 export default function Greeting() {
     return (
         <div className={styles.greeting}>
-            <p class="mb-3 font-light text-gray-500 dark:text-gray-400">
+            <p className="mb-3 font-light text-gray-500 dark:text-gray-400">
                 Lorem Ipsum - это текст-"рыба", часто используемый в печати
                 и вэб-дизайне.Lorem Ipsum является стандартной "рыбой" для
                 текстов на латинице с начала XVI века.В то время некий безымянный
