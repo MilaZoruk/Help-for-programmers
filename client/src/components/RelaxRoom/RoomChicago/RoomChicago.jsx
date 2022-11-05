52b2444f65e6f43355e7223503e2b11ca784a0bd
Lyudmila <zoruk.ll@gmail.com>
2022-11-05 16:43:09 +0300

    add Link and CSS to relax room

--- client/src/components/RelaxRoom/RoomChicago/RoomChicago.jsx
+++ client/src/components/RelaxRoom/RoomChicago/RoomChicago.jsx
@@ -53,9 +53,9 @@ export default function RoomChicago() {
             />
             <br></br>
             <div key={el.id}>
-            <p className={styles.wordDescription1}> Название:</p> <p className={styles.wordDescription}>{el.title}.</p>
-            <p className={styles.wordDescription1}>Место:</p> <p className={styles.wordDescription}>{el.place_of_origin}.</p>
-            <p className={styles.wordDescription1}>Период:</p> <p className={styles.wordDescription}>{el.date_start}-{el.date_end}.</p>
+            <p className={styles.wordDescription1}> Название:</p> <p className={styles.wordDescription}>{el.title}</p>
+            <p className={styles.wordDescription1}>Место:</p> <p className={styles.wordDescription}>{el.place_of_origin}</p>
+            <p className={styles.wordDescription1}>Период:</p> <p className={styles.wordDescription}>{el.date_start}-{el.date_end}</p>
           </div>
           </>
       ))}
