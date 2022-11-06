e7f31a49ab02588738269cfbbaea47eaa85f4d9d
Lyudmila <zoruk.ll@gmail.com>
2022-11-06 14:23:19 +0300

    add new styles

--- client/src/components/UI/Navbar/Navbar.jsx
+++ client/src/components/UI/Navbar/Navbar.jsx
@@ -60,7 +60,7 @@ export default function Navbar() {
         <Disclosure as="nav" className="bg-gray-800 w-full">
           {({ open }) => (
             <>
-              <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+              <div className="mx-auto max-w-9xl px-6 sm:px-6 lg:px-16 py-2">
                 <div className="flex h-16 items-center justify-between">
                   <div className="flex items-center">
                     <div className="flex-shrink-0">
@@ -128,7 +128,7 @@ export default function Navbar() {
                             leaveFrom="transform opacity-100 scale-100"
                             leaveTo="transform opacity-0 scale-95"
                           >
-                            <Menu.Items className="relative right-0 z-5 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                               <Menu.Item>
                                 <Link
                                   to="/profile"
