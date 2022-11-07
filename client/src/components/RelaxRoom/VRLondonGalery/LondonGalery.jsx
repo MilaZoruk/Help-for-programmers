abb93321c7b8d1d5b34121f3aa7ad73128f66326
Lyudmila <zoruk.ll@gmail.com>
2022-11-07 18:24:26 +0300

    add virtual gallery (iframe)

---
+++ client/src/components/RelaxRoom/VRLondonGalery/LondonGalery.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Iframe from 'react-iframe'
+
+export default function LondonGalery() {
+  return (
+    <Iframe
+        url="https://jubilee.moyosaspaces.com/"
+        width="100%"
+        height="100%"
+        id=""
+        className=""
+        display="block"
+        position="relative"
+        allow="fullscreen"/>
+  )
+}
---
+++ client/src/components/RelaxRoom/VRRussianMuseum/RussianGallery.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Iframe from 'react-iframe'
+
+export default function RussianGalery() {
+  return (
+    <Iframe
+        url="https://rusmuseumvrm.ru/data/vtours/ekaterina_velikaya/index.html?lp=1&lang=ru"
+        width="100%"
+        height="100%"
+        id=""
+        className=""
+        display="block"
+        position="relative"
+        allow="fullscreen"/>
+  )
+}
