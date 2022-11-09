f8db3eeba1ad475d04010c6f69b8ee41b335cf9d
Lyudmila <zoruk.ll@gmail.com>
2022-11-09 10:25:35 +0300

    css dance

--- client/src/components/Dance/Dance.jsx
+++ client/src/components/Dance/Dance.jsx
@@ -5,17 +5,17 @@ import 'react-h5-audio-player/lib/styles.css';
 
 const Dance = () => {
     return (
-  <><div className={style.body}></div>
-  
+  <><div className={style.body}>
             <AudioPlayer
             className={style.player}
-            autoPlay
+            autoPlay="true"
             // src="https://hanzluo.s3-us-west-1.amazonaws.com/music/wuyuwuqing.mp3"
             src="https://zvukipro.com/uploads/files/2022-08/1661517317_i-like-to-move-it-origina.mp3"
             onPlay={e => console.log("onPlay")} />
-            <div></div>
+            <div className={style.back}></div>
             <div className={style.floor}></div>
             <img src="https://i.imgur.com/pXALzSc.gif" className={style.wootdance} width="328" height="272" alt="8-bit dancing Karateka guy"></img>
+            </div>
     </>
     );
 };
